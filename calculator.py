5c5ed2258c697ac23f33a4befd15a5519ac1c07d
niharika48 <niharikasiri2006@gmail.com>
2025-07-07 14:39:24 +0530

    created additon subraction code

--- calculator.py
+++ calculator.py
@@ -3,4 +3,17 @@ print("b = difference")
 print("c = product")
 print("o = division")
 
+m = input("Enter your operator: ")
+
+if m == "a":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    y = z + j
+    print(y, "is the sum")
+
+elif m == "b":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    y = z - j
+    print(y, "is the difference")
 
